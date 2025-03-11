a363af65fe866853b91d68d40c4ee23bbeb21f4e
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-11 10:50:16 +0900

    Create led.cpp

---
+++ week02/led.cpp
@@ -0,0 +1,19 @@
+// C++ code
+//
+int led_r = 7;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led_r, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(led_r, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(led_r, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
