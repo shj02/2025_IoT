7854b1a372e49ebbaf49526e1d25558f265ec7a0
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-25 19:09:54 +0900

    Update lcd.ino

--- week04/lcd.ino
+++ week04/lcd.ino
@@ -5,7 +5,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  lcd.init();			//L2C LCD 포기화
+  lcd.init();			//L2C LCD 초기화
   lcd.backlight();		//백라이트 켜기
   lcd.print("LCD init");	
   delay (2000);
@@ -14,7 +14,7 @@ void setup()
 
 void loop()
 {
-  lcd.setCursor(16,0);
+  lcd.setCursor(16,0); //화면 바깥 오른쪽, 첫 번째 줄(위)
   lcd.print("Hello, World!");
     
   for (int position = 0; position < 16; position++) {
