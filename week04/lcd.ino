841df0c3839a9e82b74b22a5d09a05368e09ca22
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-25 11:22:52 +0900

    lcd

---
+++ week04/lcd.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>				//I2C 통신을 위한 기본 라이브러리
+#include <LiquidCrystal_I2C.h>	//I2C LCD 라이브러리
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+  lcd.init();			//L2C LCD 포기화
+  lcd.backlight();		//백라이트 켜기
+  lcd.print("LCD init");	
+  delay (2000);
+  lcd.clear();
+}
+
+void loop()
+{
+  lcd.setCursor(16,0);
+  lcd.print("Hello, World!");
+    
+  for (int position = 0; position < 16; position++) {
+    lcd.scrollDisplayLeft();
+    delay(150);
+  }
+}
