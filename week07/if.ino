9b8cbecacc4bc4fcb3f0bc1836ff6ee508e661cb
shj0218 <163235571+shj02@users.noreply.github.com>
2025-04-15 10:29:04 +0900

    1초마다 if블록 실행하기

---
+++ week07/if.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+}
+
+void loop() {
+  static unsigned long loopCnt = 0;
+  static unsigned long nextMil = millis() + 1000;
+
+  loopCnt++; //loopCnt = loopCnt + 1;
+  if(millis() > nextMil) {
+    nextMil = millis() + 1000;
+    Serial.println(loopCnt);
+    loopCnt = 0;
+  }
+}
