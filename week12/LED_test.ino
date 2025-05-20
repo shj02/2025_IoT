24cff71ce0887fb75aaae065e6de54e0676330e7
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-20 10:32:50 +0900

    Create LED_test.ino

---
+++ week12/LED_test.ino
@@ -0,0 +1,28 @@
+int Vo = A0;
+int V_LED = 12;
+
+float Vo_value=0;
+float Voltage = 0;
+float dustDensity = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(V_LED, OUTPUT);
+  pinMode(Vo, INPUT);
+}
+
+void loop() {
+  digitalWrite(V_LED, LOW);
+  delayMicroseconds(280);
+  Vo_value = analogRead(Vo);
+  delayMicroseconds(40);
+  digitalWrite(V_LED, HIGH);
+  delayMicroseconds(9680);
+
+  Voltage = Vo_value*5.0 / 1023.0;
+  dustDensity = (Voltage - 0.5) / 0.005;
+
+  Serial.println(dustDensity);
+
+  delay(1000);
+}
